05fe06d7e265f06709dbf3a518bddba2cc0e5a89
yuri.silenok <yuri.silenok@gmail.com>
2024-06-26 18:35:30 +0300

    Добавлены модели

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from peewee import SqliteDatabase, Model, CharField
+from peewee import SqliteDatabase, Model, ForeignKeyField, CharField, DateField, IntegerField
 
 db = SqliteDatabase('db.db', pragmas={
     'foreign_keys': 1,
@@ -12,13 +12,52 @@ class BModel(Model):
 class Group(BModel):
     name = CharField(max_length=10)
 
+class SubGroup(BModel):
+    name = CharField(max_length=10)
+    group = ForeignKeyField(model=Group)
+
 class Auditorium(BModel):
     name = CharField(max_length=5)
 
 class Discipline(BModel):
     name = CharField(max_length=100)
 
+class Teacher(BModel):
+    fullname = CharField(max_length=255)
+
+class Lesson(BModel):
+    date = DateField()
+    number = IntegerField()
+
+class LessonGroup(BModel):
+    lesson = ForeignKeyField(Lesson)
+    group = ForeignKeyField(Group)
+
+class LessonSubGroup(BModel):
+    lesson = ForeignKeyField(Lesson)
+    sub_group = ForeignKeyField(SubGroup)
+
+class LessonAuditorium(BModel):
+    lesson = ForeignKeyField(Lesson)
+    auditorium = ForeignKeyField(Auditorium)
+
+class LessonTeacher(BModel):
+    lesson = ForeignKeyField(Lesson)
+    teacher = ForeignKeyField(Teacher)
+
 db.connect()
 db.create_tables(
-    [Group], safe=True)
+    [
+        Group, 
+        SubGroup, 
+        Auditorium, 
+        Discipline, 
+        Teacher, 
+        Lesson, 
+        LessonAuditorium, 
+        LessonGroup, 
+        LessonSubGroup, 
+        LessonTeacher
+    ], 
+    safe=True)
 db.close()
