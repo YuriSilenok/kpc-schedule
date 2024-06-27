a882c3c2abf83dc05eca1bc4cd1097dd9bf2f5c6
yuri.silenok <yuri.silenok@gmail.com>
2024-06-27 15:20:36 +0300

    Занятие 003

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
 from fastapi import FastAPI
-import models
+import models as m
+import base_models as bm
 
 app = FastAPI()
 
 @app.get("/groups")
-async def read_root():
+async def get_groups():
+    '''Получить список групп'''
     result = {
         'count':0,
         'entities':[],
     }
-    for group in models.Group.select():
+    for group in m.Group.select():
         result['count'] += 1
         result['entities'].append({
             'id': group.id,
             'name': group.name,
+        })
+    return result
+
+@app.post("/groups")
+async def post_groups(group: bm.Group):
+    '''Создать группу'''
+    m.Group.create(
+        name=group.name
+    )
+    return group
+    
+
+@app.get("/subgroups")
+async def get_sub_groups():
+    '''Получить список подгрупп'''
+    result = {
+        'count':0,
+        'entities':[],
+    }
+    for sub_group in m.SubGroup.select():
+        result['count'] += 1
+        result['entities'].append({
+            'id': sub_group.id,
+            'name': sub_group.name,
+            'grou_id': sub_group.group_id
         })
     return result
